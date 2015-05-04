f3528cecb45e429f8c0be63dcb72b1b55d58ac68
thelaughingcheese <jiasheng95@yahoo.ca>
2015-05-04 10:20:52 -0400

    config struct created

--- EscConfiguration.h
+++ EscConfiguration.h
@@ -2,7 +2,51 @@
 #define ESCCONFIGURATION
 
 class EscConfiguration{
+private:
+public:
+	typedef enum{
+		ON,
+		OFF
+	} Brake;
+	typedef enum{
+		LIPO,
+		NICD
+	} BatteryType;
+	typedef enum{
+		SOFT,
+		CUTOFF
+	} CutoffMode;
+	typedef enum{
+		LOW,
+		MEDIUM,
+		HIGH
+	} CutoffThreshold;
+	typedef enum{
+		NORMAL,
+		SOFT,
+		SUPERSOFT
+	} StartMode;
+	typedef enum{
+		LOW,
+		MEDIUM,
+		HIGH
+	} Timing;
 
+	Brake brake;
+	BatteryType batteryType;
+	CutoffMode cutoffMode;
+	CutoffThreshold cutoffThreshold;
+	StartMode startmode;
+	Timing timing;
+
+	EscConfiguration(){
+		brake = OFF;
+		batteryType = LIPO;
+		cutoffMode = CUTOFF;
+		cutoffThreshold = MEDIUM;
+		startmode = SOFT;
+		timing = LOW;
+	}
 };
 
 #endif
